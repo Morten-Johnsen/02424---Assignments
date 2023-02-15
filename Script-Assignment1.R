af712044ba9264d615d67c8b6ac31970425cefad
CathrineLind <s184338@student.dtu.dk>
2023-02-15 08:39:45 +0100

    Update Script-Assignment1.R
    setwd() for Cathrine

--- Script-Assignment1.R
+++ Script-Assignment1.R
@@ -7,6 +7,8 @@ if (Sys.getenv('USER') == "mortenjohnsen"){
   setwd("/Users/mortenjohnsen/OneDrive - Danmarks Tekniske Universitet/DTU/10. Semester/02424 - Advanced Dataanalysis and Statistical Modellling/02424---Assignments/")
 }else if (Sys.getenv('USER') == "freja"){
   setwd("~/Documents/Uni/TiendeSemester/Adv. data analysis and stat. modelling/02424---Assignments")
+}else{
+  setwd("C:/Users/catdu/OneDrive/DTU/10. semester/Advanced Dataanalysis and Statistical Modelling/Assignment 1/02424---Assignments/")
 }
 
 dioxin <- read.csv("./dioxin.csv")
