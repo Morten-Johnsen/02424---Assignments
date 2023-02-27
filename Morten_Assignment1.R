fbe5455389fb7dca0ecb0aa96411cb6150bf38fc
Morten-Johnsen <s184334@dtu.dk>
2023-02-27 16:48:20 +0100

    Update Morten_Assignment1.R

--- Morten_Assignment1.R
+++ Morten_Assignment1.R
@@ -22,72 +22,87 @@ names(dioxin)
 #Active variables: 
 #   "Theoretical": OXYGEN, LOAD, PRSEK.
 #   "Measured": O2 (O2COR), NEFFEKT, QRAT
-dioxin$plant <- factor(dioxin$PLANT)
-dioxin$time <- factor(dioxin$TIME)
-dioxin$lab <- factor(dioxin$LAB_USA_or_KK)
-dioxin$oxygen <- factor(dioxin$OXYGEN)
-dioxin$load <- factor(dioxin$LOAD)
-dioxin$prsek <- factor(dioxin$PRSEK)
-
-
 #### 2) ####
-fit <- lm(logDiox ~ LOAD_Ordinal*OXYGEN_Ordinal*PRSEK_Ordinal + plant + time + lab, data = dioxin)
-Anova(fit, type = "III")
-summary(fit)
+fit <- lm(logDiox ~ PLANT + TIME + LAB+LOAD_Ordinal + OXYGEN_Ordinal + PRSEK_Ordinal, data = dioxin)
 #plot(fit)
 drop1(fit, test = "F")
-fit2 <- update(fit, .~.-LOAD_Ordinal:OXYGEN_Ordinal:PRSEK_Ordinal)
+fit2 <- update(fit, .~.-PRSEK_Ordinal)
 drop1(fit2, test = "F")
-fit3 <- update(fit2, .~.-LOAD_Ordinal:OXYGEN_Ordinal)
-drop1(fit3, test = "F")
-fit4 <- update(fit3, .~.-LOAD_Ordinal:PRSEK_Ordinal)
-drop1(fit4, test = "F")
-fit5 <- update(fit4, .~.-OXYGEN_Ordinal:PRSEK_Ordinal)
-drop1(fit5, test = "F")
-fit6 <- update(fit5, .~.-PRSEK_Ordinal)
-drop1(fit6, test = "F")
 
-anova(fit, fit6)
-Anova(fit6, type = "III")
+anova(fit, fit2)
+Anova(fit2, type = "III")
+summary(fit2)
 
 par(mfrow=c(2,2))
-plot(fit6)
+plot(fit2)
 
 #### 3) ####
-dioxin %>%
-  dplyr::select(O2COR, NEFFEKT, QRAT, plant, time, lab, logDiox) %>%
-  ggpairs()
-fit_obs <- lm(logDiox ~ O2COR*NEFFEKT*QRAT + plant + time + lab, data = dioxin)
+fit_obs <- lm(logDiox ~ O2COR + NEFFEKT + QRAT + PLANT + TIME + LAB, data = dioxin)
 Anova(fit_obs, type = "III")
 
 drop1(fit_obs, test = "F")
-fit_obs1 <- update(fit_obs, .~.-O2COR:NEFFEKT:QRAT)
+fit_obs1 <- update(fit_obs, .~.-QRAT)
 drop1(fit_obs1, test = "F")
-fit_obs2 <- update(fit_obs1, .~.-O2COR:NEFFEKT)
-drop1(fit_obs2, test = "F")
-fit_obs3 <- update(fit_obs2, .~.-O2COR:QRAT)
-drop1(fit_obs3, test = "F")
-fit_obs4 <- update(fit_obs3, .~.-NEFFEKT:QRAT)
-drop1(fit_obs4, test = "F")
-fit_obs5 <- update(fit_obs4, .~.-QRAT)
-drop1(fit_obs5, test = "F")
 
-anova(fit_obs, fit_obs5) #model performance are the same
-Anova(fit_obs5, type = "III") #type 3 anova
+anova(fit_obs, fit_obs1) #model performance are the same
+Anova(fit_obs1, type = "III") #type 3 anova
 
-summary(fit_obs5)
-plot(fit_obs5)
+summary(fit_obs1)
+plot(fit_obs1)
 par(mfrow=c(1,1))
-qqPlot(fit_obs5)
-confint(fit_obs5, level = 0.99)
+qqPlot(fit_obs1)
+confint(fit_obs1, level = 0.99)
 
 dioxin[c(13,24,11,20),] %>%
-  dplyr::select(plant, time, lab, NEFFEKT, O2COR, logDiox)
+  dplyr::select(PLANT, LAB, TIME, NEFFEKT, O2COR, logDiox)
 
 dioxin$outlier <- "No"
 dioxin$outlier[c(13,24,11,20)] <- "Yes"
 dioxin$chrLog <- as.character(round(dioxin$logDiox,2))
 library(gghighlight)
 ggplot(dioxin)+
-  geom_point(aes(x = OBSERV, y = logDiox, fill = outlier))+
+  geom_point(aes(x = PRSEK, y = logDiox, fill = outlier))+
   gghighlight(outlier == "Yes", label_key = chrLog)
+
+#4)
+predict(fit_obs1, newdata = data.frame("PLANT" = factor("RENO_N"), 
+                                       "LAB" = factor("KK"),
+                                       "TIME" = factor(1),
+                                       "O2COR" = 0.5,
+                                       "NEFFEKT" = -0.01), 
+        interval = "prediction",
+        level = 0.95)
+
+#5)
+AIC(fit2)
+AIC(fit_obs1)
+#Operating conditions make a difference.
+coef(fit2)
+coef(fit_obs1)
+#dioxin emission depend on the operating conditions of oxygen (O2COR) and load (NEFFEKT)
+#but not on PRSEK (QRAT). 
+#Dioxin emission can be reduced by reducing oxygen and load.
+
+
+#6)
+#Differences between plants:
+#PLANT_S predicts -2.11 log(ppm) less than plant KARA
+#PLANT_N predicts -0.57 log(ppm) less than plant KARA
+
+#7)
+par(mfrow=c(2,3))
+plot(dioxin$O2COR, fit_obs1$residuals)
+plot(dioxin$NEFFEKT, fit_obs1$residuals)
+plot(dioxin$TIME, fit_obs1$residuals)
+plot(dioxin$PLANT, fit_obs1$residuals)
+plot(dioxin$LAB, fit_obs1$residuals)
+
+
+add1(fit_obs1, scope = ~.+O2COR*NEFFEKT+I(O2COR^2)+I(NEFFEKT^2)+QROEG+TOVN+TROEG+POVN+CO2+CO+SO2+HCL+H2O+I(QROEG^2)+I(TOVN^2)+I(TROEG^2)+I(POVN^2)+I(CO2^2)+I(CO^2)+I(SO2^2)+I(HCL^2)+I(H2O^2)
+     , test = "F", data = dioxin)
+model <- update(fit_obs1, .~.+HCL, data = dioxin)
+add1(model, scope = ~.+I(O2COR^2)+I(NEFFEKT^2)+QROEG+TOVN+TROEG+POVN+CO2+CO+SO2+H2O+I(QROEG^2)+I(TOVN^2)+I(TROEG^2)+I(POVN^2)+I(CO2^2)+I(CO^2)+I(SO2^2)+I(HCL^2)+I(H2O^2), test = "F", data = dioxin)
+drop1(model, test = "F")
+
+par(mfrow = c(2,2))
+plot(model)
