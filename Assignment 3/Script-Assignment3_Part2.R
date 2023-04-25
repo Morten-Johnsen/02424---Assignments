1ebd78ef8b18ff0565926da276e9fe4a0c23d0c4
Morten-Johnsen <s184334@dtu.dk>
2023-04-25 09:45:59 +0200

    dqw

--- Assignment 3/Script-Assignment3_Part2.R
+++ Assignment 3/Script-Assignment3_Part2.R
@@ -1,5 +1,6 @@
-#### Part 2 ####
+######################## Part 2 ########################
 library(lme4)
+#### Part 2.1 ####
 fit0 <- lmer(clo~sex+(1|subjId),data=c.data,REML=FALSE)
 summary(fit0)
 
@@ -26,7 +27,12 @@ opt.fun <- function(theta){
 par_est <- nlminb(start = c(log(0.11665),log(0.09883), 0.59176, -0.08322),
       objective = opt.fun, control = list(trace = 1))
 sqrt(exp(par_est$par[1:2]))
-
+manual_fit0 <- data.frame("Parameter" = c("Psi (sigma.u)", "Sigma", "beta0", "beta1"),
+                          "Interpretation" = c("SubjId", "Model Residual", "Model intercept", "Model slope (sex)"),
+                          "Estimate" = c(sqrt(exp(par_est$par[1:2])), par_est$par[3:4]))
+cat("Log-likelihood = ", -par_est$objective)
+manual_fit0
+fit0
 Psi <- diag(rep(exp(par_est$par[1]),dim(Z)[2]))
 Sigma <- diag(rep(exp(par_est$par[2]),length(y)))
 beta <- matrix(par_est$par[3:4], ncol = 1)
@@ -35,8 +41,68 @@ y_adj <- y - X%*%beta
 u <- solve(t(Z)%*%solve(Sigma)%*%Z + solve(Psi)) %*% (t(Z)%*%solve(Sigma)%*%y_adj)
 cbind(ranef(fit0)$subjId, u)
 
+#### 2.2 ####
+fit1 <- lmer(clo~sex+(1|subjId)+(1|subjId:day),data=c.data,REML=FALSE)
+ranef(fit1)
 
+X <- cbind(1, as.numeric(c.data$sex == "male"))
+Z <- dummy(c.data$subjId, levelsToKeep = unique(c.data$subjId))
+#Introduce W which is the design matrix for the nested subjdID:day observations
+c.data$subjDay <- factor(paste0(c.data$subjId, "/",c.data$day))
+W <- dummy(c.data$subjDay, levelsToKeep = unique(c.data$subjDay))
+y <- c.data$clo
+dim(Z)
 
+#### Mere simpel tilgang med bare at skrive density og likelihood op
+
+opt.fun <- function(theta){
+  Psi <- diag(rep(exp(theta[2]),dim(Z)[2]))
+  Sigma <- diag(rep(exp(theta[3]),length(y)))
+  Phi <- diag(rep(exp(theta[1]),dim(W)[2]))
+  
+  beta <- matrix(theta[4:5], ncol = 1)
+  
+  V <- Sigma + Z%*%Psi%*%t(Z) + W%*%Phi%*%t(W)
+  
+  obj <-  mvtnorm::dmvnorm(y, mean = X%*%beta, sigma = V, log = T)
+  
+  return(-obj)
+}
+
+par_est1 <- nlminb(start = c(0, 0, 0, 1, 1),
+                  objective = opt.fun, control = list(trace = 1))
+#Sammenlign med fit1
+fit1
+manual_fit1 <- data.frame("Parameter" = c("Phi (sigma.v)", "Psi (sigma.u)", "Sigma", "beta0", "beta1"),
+                          "Interpretation" = c("SubjId/day", "SubjId", "Model Residual", "Model Intercept", "Model Slope (sex)"),
+                      "Estimate" = c(sqrt(exp(par_est1$par[1:3])), par_est1$par[4:5]))
+cat("Log-likelihood = ", -par_est1$objective)
+manual_fit1
+
+#Hierarchical estimation: We first estimated Beta, Psi and Phi.
+#now we will estimate u and then the nested value v.
+Phi <- diag(rep(exp(par_est1$par[1]),dim(W)[2]))
+Psi <- diag(rep(exp(par_est1$par[2]),dim(Z)[2]))
+Sigma <- diag(rep(exp(par_est1$par[3]),length(y)))
+beta <- matrix(par_est1$par[4:5], ncol = 1)
+
+inner_opt <- function(omega, Phi, Psi, Sigma, beta, X, y){
+  u <- matrix(omega[1:dim(Z)[2]], ncol = 1)
+  v <- matrix(omega[-c(1:dim(Z)[2])], ncol = 1)
+  
+  
+  V <- Sigma + Z%*%Psi%*%t(Z) + W%*%Phi%*%t(W)
+  
+  obj <- dmvnorm(y, X%*%beta - Z%*%u - W%*%v, sigma = V, log = T)
+  return(-obj)
+}
+
+opt_u_and_v <- nlminb(start = rep(0, dim(W)[2]+dim(Z)[2]), objective = inner_opt,
+                      Phi = Phi, Psi = Psi, Sigma = Sigma, beta = beta, X = X, y = y,
+                      control = list(trace = 1))
+
+cbind(ranef(fit1)$subjId, opt_u_and_v$par[1:dim(Z)[2]])
+cbind(ranef(fit1)$`subjId:day`, opt_u_and_v$par[-c(1:dim(Z)[2])])
 ###### GAMMELT #####
 #Simultaneous estimation of beta and u for known variances p. 184
 beta <- solve(t(X)%*%X)%*%t(X)%*%y
