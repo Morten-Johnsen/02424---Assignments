678ad6c048d8aced18259e95452d2d91b8f88b39
FrejaTerpPetersen <s184321@dtu.student.dk>
2023-04-27 14:32:52 +0200

    One comparison

--- Assignment 3/Script-Assignment3_Part1.R
+++ Assignment 3/Script-Assignment3_Part1.R
@@ -276,6 +276,10 @@ anova(fit.mm.nest.fwREML) # This test does also not take random effects into acc
 final.model.REML2 <- fit.mm.nest.fwREML
 final.model.ML2 <- fit.mm.nest3
 
+# Compare with model from 1.2:
+
+anova(final.model.ML2,final.model.ML1)
+# Conclusion: final.model.ML2 is better
 
 
 # Visualise ---------------------------------------------------------------
@@ -303,7 +307,7 @@ plot(final.model.ML2, clo ~ fitted(.) | sex, abline = c(0,1))
 # subjId is random effect
 # Only consider random intercept - not slope AND intercept
 
-# Forward selectiong:
+# Forward selection:
 # insists that the grouping variable for the random effects and for the correlation be the same
 fit.mm.autocor <- lme(clo ~ tOut,
                       random = ~1|subDay,
